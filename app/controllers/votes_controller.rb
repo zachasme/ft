914f397363beadadc8b62bf53b62984df2adec4b
Zacharias Knudsen <z@chari.as>
2024-06-09 20:01:19 +0200

    Initial commit

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+
+class VotesController < ApplicationController
+  def index
+    url = URI.parse('https://oda.ft.dk/api/Afstemning?$expand=Sagstrin/Sag,Stemme/Stemmetype,Stemme/Akt%C3%B8r&$orderby=id desc&$top=20')
+    @response = JSON.parse(URI.open(url).read)
+  end
+end
