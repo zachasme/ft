217a2b04c7db65ce55cbfa44d65de3c1aeae9272
Zacharias Knudsen <z@chari.as>
2024-06-09 20:35:21 +0200

    More pretty

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 class VotesController < ApplicationController
   def index
-    url = URI.parse('https://oda.ft.dk/api/Afstemning?$expand=Sagstrin/Sag,Stemme/Stemmetype,Stemme/Akt%C3%B8r&$orderby=id desc&$top=20')
+    url = URI.parse('https://oda.ft.dk/api/Afstemning?$expand=Sagstrin/Sag,Stemme/Stemmetype,Stemme/Akt%C3%B8r&$orderby=id desc&$top=10')
     @response = JSON.parse(URI.open(url).read)
   end
 end
