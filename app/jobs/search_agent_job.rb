682fbfb9b3b4e8853255981f6f40697e9eeace1f
Zacharias Knudsen <z@chari.as>
2024-09-09 15:22:32 +0200

    Search agent only when new

--- app/jobs/search_agent_job.rb
+++ app/jobs/search_agent_job.rb
@@ -2,9 +2,10 @@ class SearchAgentJob < ApplicationJob
   def perform(search_agent)
     search_agent.transaction do
       cases = search_agent.search
-      SearchAgentMailer.with(search_agent:).notify(*cases).deliver_later
-
-      search_agent.update! executed_at: Time.current
+      if cases.present?
+        SearchAgentMailer.with(search_agent:).notify(*cases).deliver_later
+        search_agent.update! executed_at: Time.current
+      end
     end
   end
 end
