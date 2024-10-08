b4b6e62ee4b5d06f12b8735a859b2fbd569976f2
Zacharias Knudsen <z@chari.as>
2024-10-08 09:25:17 +0200

    Fix mailer

--- app/jobs/search_agent_job.rb
+++ app/jobs/search_agent_job.rb
@@ -2,7 +2,7 @@ class SearchAgentJob < ApplicationJob
   def perform(search_agent)
     search_agent.transaction do
       cases = search_agent.search
-      SearchAgentMailer.with(search_agent).notify(*cases).deliver_later
+      SearchAgentMailer.with(search_agent:).notify(*cases).deliver_later
 
       search_agent.update! executed_at: Time.current
     end
