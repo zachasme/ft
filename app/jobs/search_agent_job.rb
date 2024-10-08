2421c083814a8f61b2782be58dfd05d351a459ee
Zacharias Knudsen <z@chari.as>
2024-10-08 09:42:37 +0200

    No empty mails

--- app/jobs/search_agent_job.rb
+++ app/jobs/search_agent_job.rb
@@ -2,7 +2,9 @@ class SearchAgentJob < ApplicationJob
   def perform(search_agent)
     search_agent.transaction do
       cases = search_agent.search
-      SearchAgentMailer.with(search_agent:).notify(*cases).deliver_later
+      if cases.present?
+        SearchAgentMailer.with(search_agent:).notify(*cases).deliver_later
+      end
 
       search_agent.update! executed_at: Time.current
     end
--- app/mailers/search_agent_mailer.rb
+++ app/mailers/search_agent_mailer.rb
@@ -2,6 +2,6 @@ class SearchAgentMailer < ApplicationMailer
   def notify(*cases)
     @search_agent = params[:search_agent]
     @cases = cases
-    mail subject: "Nye sager i folketinget matcher din søgeagetn", to: @search_agent.user.email_address
+    mail subject: "Nye sager i folketinget matcher din søgeagent", to: @search_agent.user.email_address
   end
 end
