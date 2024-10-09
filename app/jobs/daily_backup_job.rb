1dadbf1e8ea120d8827af70abe3b672e14b437dd
Zacharias Knudsen <z@chari.as>
2024-10-09 14:12:19 +0200

    Daily backup job

--- app/jobs/daily_backup_job.rb
+++ app/jobs/daily_backup_job.rb
@@ -1,5 +1,16 @@
 class DailyBackupJob < ApplicationJob
   def perform
-    `sqlite storage/oda_production.sqlite3 ".backup 'tmp/storage/oda_production-$(date '+%Y-%m-%d-%H-%M-%S').sqlite3'"`
+    filename = "production-#{Date.current}.sqlite3"
+    filepath = "tmp/storage/#{filename}"
+
+    raise "backup failed" unless system "sqlite3 storage/production.sqlite3 '.backup #{filepath}'"
+    raise "gzip failed "  unless system "gzip --force #{filepath}"
+    raise "gpg failed"    unless system "gpg --yes --batch --passphrase='#{Rails.application.credentials.backup_passphrase}' --output '#{filepath}.gz.gpg' -c '#{filepath}.gz'"
+    raise "curl failed"   unless system "curl --aws-sigv4 'aws:amz:auto:s3' \
+                                              --user '#{Rails.application.credentials.backup_access_key_id}:#{Rails.application.credentials.backup_secret_access_key}' \
+                                              --upload-file #{filepath}.gz.gpg \
+                                              #{Rails.application.credentials.backup_bucket}/#{filename}.gz.gpg"
+
+    puts "backup complete!"
   end
 end
