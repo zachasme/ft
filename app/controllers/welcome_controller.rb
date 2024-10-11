997fcc34623a5f216101d8d56f8a797edbbb979d
Zacharias Knudsen <z@chari.as>
2024-10-11 14:57:47 +0200

    New welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def show
+    @latest = Oda::Afstemning.chronological.first(5)
   end
 end
--- app/helpers/highlight_helper.rb
+++ app/helpers/highlight_helper.rb
@@ -1,9 +1,15 @@
 module HighlightHelper
   def highlighted_excerpt(haystack, needle)
-    if needle.present?
+    highlighted = if needle.present?
       highlight(excerpt(haystack, needle, radius: 30), needle)
     else
       truncate(haystack, length: 60)
+    end
+    
+    if highlighted.empty?
+      truncate(haystack, length: 60)
+    else
+      highlighted
     end
   end
 end
