4473d3077c9856cca493ac0630f9c795e5472ec6
Zacharias Knudsen <z@chari.as>
2024-10-11 11:22:48 +0200

    relationer

--- app/models/oda/aktør.rb
+++ app/models/oda/aktør.rb
@@ -3,10 +3,11 @@ class Oda::Aktør < Oda::OdaRecord
 
   has_many :stemmer
   has_many :afstemninger, through: :stemmer
+
   has_many :fra_aktør_aktører, foreign_key: :fraaktør_id, class_name: "Oda::AktørAktør"
   has_many :til_aktør_aktører, foreign_key: :tilaktør_id, class_name: "Oda::AktørAktør"
-  has_many :fraaktører, through: :fra_aktør_aktører
-  has_many :tilaktører, through: :til_aktør_aktører
+  has_many :fraaktører, through: :til_aktør_aktører
+  has_many :tilaktører, through: :fra_aktør_aktører
 
   belongs_to :type, class_name: "Oda::Aktørtype"
   belongs_to :periode, optional: true, counter_cache: true
@@ -15,6 +16,13 @@ class Oda::Aktør < Oda::OdaRecord
   scope :chronological, -> { order(opdateringsdato: :desc) }
   scope :matches, ->(search) { where("oda_aktører.navn Like ?", "%#{search}%") if search.present? }
   scope :actortype, ->(id) { joins(:type).where(type: { id: }) }
+
+  def relationer(rolle)
+    from = fraaktører.where(oda_aktør_aktører: { rolle_id: rolle.id })
+    to   = tilaktører.where(oda_aktør_aktører: { rolle_id: rolle.id })
+    (from + to).uniq
+  end
+  
 
   def picture
     nokogiri.css("pictureMiRes").first.content
