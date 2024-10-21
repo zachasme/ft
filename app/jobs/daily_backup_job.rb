5be35cace265ccc031ec5594a7f47d1f5f49c3f0
Zacharias Knudsen <z@chari.as>
2024-10-21 11:54:51 +0200

    Return filepath from backup job

--- app/jobs/daily_backup_job.rb
+++ app/jobs/daily_backup_job.rb
@@ -16,6 +16,6 @@ class DailyBackupJob < ApplicationJob
     system "gpg --yes --batch --passphrase='#{passphrase}' --output '#{filepath}.gz.gpg' -c '#{filepath}.gz'" or raise "gpg failed"
     system "curl --aws-sigv4 'aws:amz:auto:s3' --user '#{access_key_id}:#{secret_access_key}' --upload-file #{filepath}.gz.gpg #{bucket}/#{filename}.gz.gpg" or raise "curl failed"
 
-    puts "backup complete!"
+    filepath
   end
 end
